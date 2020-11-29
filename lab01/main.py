4b605563fc59787274245da9da4f2b9a82829b50
ryanhe312 <312013477@qq.com>
2020-11-29 16:47:24 +0800

    fix lab01

--- lab01/main.py
+++ lab01/main.py
@@ -1,24 +1,59 @@
 import librosa
+
 import numpy as np
+
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
+import matplotlib.patches as patches
+import matplotlib.path as path
+import matplotlib.animation as animation
 
-chunk = 8192
+np.random.seed(19680801)
+
 y, sr = librosa.load(librosa.example('brahms'))
+S = np.abs(librosa.stft(y,len(y)//10))
 
-fig = plt.figure()
-ax = plt.subplot(111,projection='polar')
-ax.axis('off')
-ln, = ax.plot(np.linspace(0,2*np.pi,chunk),np.zeros(chunk))
+frames,bins = S.shape
+n = np.zeros(bins) + 50
+bins = np.arange(bins+1)
 
-def update(frame):
-    y_clip = y[frame:frame+chunk]
-    y_fft = librosa.amplitude_to_db(np.abs(np.fft.fft(y_clip)), ref=np.max)
-    y_fft = 0.02 * y_fft / np.max(y_fft)
-    y_fft = np.roll(y_fft,100*frame//chunk)
-    ln.set_ydata(y_fft)
-    return ln,
+left = np.array(bins[:-1])
+right = np.array(bins[1:])
+bottom = np.zeros(len(left))
+top = bottom + n
+nrects = len(left)
 
-ani = FuncAnimation(fig, update, frames = np.arange(0,len(y),chunk))
+nverts = nrects * (1 + 3 + 1)
+verts = np.zeros((nverts, 2))
+codes = np.ones(nverts, int) * path.Path.LINETO
+codes[0::5] = path.Path.MOVETO
+codes[4::5] = path.Path.CLOSEPOLY
+verts[0::5, 0] = left
+verts[0::5, 1] = bottom
+verts[1::5, 0] = left
+verts[1::5, 1] = top
+verts[2::5, 0] = right
+verts[2::5, 1] = top
+verts[3::5, 0] = right
+verts[3::5, 1] = bottom
 
+patch = None
+
+def animate(frame):
+    # simulate new data coming in
+    n = librosa.amplitude_to_db(S[frame])
+    top = bottom + n
+    verts[1::5, 1] = top
+    verts[2::5, 1] = top
+    return [patch, ]
+
+fig, ax = plt.subplots()
+barpath = path.Path(verts, codes)
+patch = patches.PathPatch(
+    barpath, facecolor='green', edgecolor='yellow', alpha=0.5)
+ax.add_patch(patch)
+
+ax.set_xlim(left[0], right[-1])
+ax.set_ylim(bottom.min(), top.max())
+
+ani = animation.FuncAnimation(fig, animate, frames)
 plt.show()
